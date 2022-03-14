f81828741e2a3ad7b76ddb981e4b0dcdb327ad91
Arun <rarnkmrbridgelabz.com>
2022-03-14 14:42:00 +0530

    Added Daily Employee Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,14 +3,20 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int empPresent= random.nextInt(2);
-        if(empPresent==0){
-            System.out.println("Employee is absent");
-        }
-        else{
-            System.out.println("Employee is Present");
+        static final int WAGE_PER_HOUR =20;
+        static final int FULL_TIME_HOUR=0;
+        public static void main(String[] args) {
+            Random random = new Random();
+            int empPresent= random.nextInt(2);
+            int empWage=0;
+            if(empPresent==0){
+                System.out.println("Employee is absent");
+            }
+            else{
+                empWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
+                System.out.println("Employee is Present");
+            }
+            System.out.println("Employee Wage:"+empWage);
         }
     }
-}
+
