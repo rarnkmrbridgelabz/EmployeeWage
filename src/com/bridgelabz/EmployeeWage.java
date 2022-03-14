9c66a43e53d585433556846117757e1ed7b127e9
Arun <rarnkmrbridgelabz.com>
2022-03-14 14:51:21 +0530

    Added PartTime Employee & Wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,20 +3,24 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWage {
-        static final int WAGE_PER_HOUR =20;
-        static final int FULL_TIME_HOUR=0;
-        public static void main(String[] args) {
-            Random random = new Random();
-            int empPresent= random.nextInt(2);
-            int empWage=0;
-            if(empPresent==0){
-                System.out.println("Employee is absent");
-            }
-            else{
-                empWage=WAGE_PER_HOUR*FULL_TIME_HOUR;
-                System.out.println("Employee is Present");
-            }
-            System.out.println("Employee Wage:"+empWage);
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_TIME_HOUR = 8;
+    static final int PART_TIME_HOUR = 4;
+    static final int IS_PART_TIME = 1;
+    static final int IS_FULL_TIME = 2;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int empPresent = random.nextInt(3);
+        int empWage = 0;
+        if (empPresent == IS_PART_TIME) {
+            empWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+        } else if (empPresent == IS_FULL_TIME) {
+            empWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+        } else {
+            empWage = 0;
         }
+        System.out.println("Employee Wage:" + empWage);
     }
+}
 
